ef61ce061e4955c4cd4f8c814e9c85e9ab4109cd
Awais Safdar <88376874+AwaisSafdar9111@users.noreply.github.com>
2022-04-05 15:38:13 +0500

    initial commit

---
+++ android/app/src/main/java/com/example/spice_bazaar/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.spice_bazaar;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Spice Bazaar',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('Spice Bazaar'),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children:  <Widget>[
+            //header
+            UserAccountsDrawerHeader(
+              accountName: const Text('Moiz Butt'),
+              accountEmail: const Text('moizbutt890@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
